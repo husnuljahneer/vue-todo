25d22833405d35cf746bbf7c2c49cd8a2d6e41cc
Husnul Jahneer <husnuljahneer19@gmail.com>
2022-01-13 12:20:39 +0530

    Created a new component

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,18 @@
+<template>
+  <div >
+   Hello World, this is working.....
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
